8e9ce747a56f2477f9fd051bac2be395d29e9b23
Rui Almeida <Rui.Almeida2@vodafone.com>
2024-04-18 23:20:15 +0100

    #RA# - First Commit

---
+++ src/Calculadora.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Primeiro numero:");
+		double x = sc.nextDouble();
+		System.out.println("Segundo numero:");
+		double y = sc.nextDouble();
+		System.out.println("Operacao (+ ,- , *, / ):");
+		String operator = sc.next();
+
+		System.out.print("Resultado: ");
+		double result =0.0;
+				
+		if(operator.equals("+")) {
+			result = sum(x,y);
+		}
+		System.out.println(result);
+	}
+	
+	public static double sum(double x, double y) {
+		return x+y;
+	}
+
+}
